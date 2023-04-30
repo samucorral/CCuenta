945de2f0218c6acd996b14325d7bb1cfe04a46ae
samucorral <samucorral97@gmail.com>
2023-04-30 13:10:41 +0200

    Agregada documentación con Javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mycompany.ccuenta","l":"CCuenta"},{"p":"com.mycompany.ccuenta","l":"Main"}];updateSearchResults();
